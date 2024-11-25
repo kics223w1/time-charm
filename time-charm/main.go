f5990ccd8b7e0ed51d1d2a15e87c7c929cb430b6
kics223w1 <hieppro138@gmail.com>
2024-11-25 15:42:48 +0700

    refactor: testcase have hideYearAndDays,hideSeconds and colonNotation options

--- time-charm/main.go
+++ time-charm/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 
+	"math/big"
+
 	"github.com/kics223w1/time-charm/timecharm"
 )
 
 func main() {
+	maxValue := new(big.Int)
+
 	options := timecharm.Options{
 		MillisecondsDecimalDigits: (4),
 	}
-	fmt.Printf("huy %s\n", timecharm.PrettyMilliseconds(float64(33.333), options))
+
+	fmt.Printf("huy %s\n", timecharm.PrettyMilliseconds((maxValue), options))
 }
